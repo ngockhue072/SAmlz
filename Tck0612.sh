032408c29d564e3f2054068b494d318e51c590e1
ilehoe0202 <ilehoe0902@gmail.com>
2017-12-07 15:45:56 +0700

    hihi

---
+++ Tck0612.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update && apt-get -y upgrade
+sudo apt-get install -y git make curl unzip gedit automake autoconf dh-autoreconf build-essential pkg-config openssh-server screen libtool libcurl4-openssl-dev libncurses5-dev libudev-dev libjansson-dev libssl-dev libgmp-dev gcc g++ screen
+git clone https://github.com/JayDDee/cpuminer-opt
+cd cpuminer-opt
+./build.sh
+cp cpuminer ../
+cd ..
+screen -d -m sudo ./cpuminer -a cryptonight -o stratum+tcp://truckcoin.ddns.net:3333 -u WcC2Wcqz6Ww1dkqF6ohxiaeL1hENdKfVjHvL3hmkh5m2FD9Z6eAJr8L6zaTPmohhmEZBgMTbH8rSpXowsisEo2cr2kJ4p3gqg
