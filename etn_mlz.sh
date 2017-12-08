9d80cefb8daa3470d650564c3f0690e9c44477e2
ilehoe0202 <ilehoe0902@gmail.com>
2017-12-08 22:47:32 +0700

    Create etn_mlz.sh

---
+++ etn_mlz.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update && apt-get -y upgrade
+sudo apt-get install -y git make curl unzip gedit automake autoconf dh-autoreconf build-essential pkg-config openssh-server screen libtool libcurl4-openssl-dev libncurses5-dev libudev-dev libjansson-dev libssl-dev libgmp-dev gcc g++ screen
+git clone https://github.com/JayDDee/cpuminer-opt
+cd cpuminer-opt
+./build.sh
+cp cpuminer ../
+cd ..
+screen -d -m sudo ./cpuminer -a cryptonight -o stratum+tcp://pool.electroneum.space:1111 -u etnkDo9T9HxL7boSM4nJaK8NBB7BCCurb416bi4CmJJgJkSin2SZpDnhh8TDNrAX6m647r7Zn64AibUHL9Mmp1kg3NRMKfDxHA -p x
