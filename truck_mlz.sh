7ed416e3e97a7f02d51b3c5a950293b26587faf2
ilehoe0202 <ilehoe0902@gmail.com>
2017-12-09 08:15:59 +0700

    Update truck_mlz.sh

--- truck_mlz.sh
+++ truck_mlz.sh
@@ -6,4 +6,4 @@ cd cpuminer-opt
 ./build.sh
 cp cpuminer ../
 cd ..
-screen -d -m sudo ./cpuminer -a cryptonight -o stratum+tcp://http://52.173.37.113:1111 -u WcC2Wcqz6Ww1dkqF6ohxiaeL1hENdKfVjHvL3hmkh5m2FD9Z6eAJr8L6zaTPmohhmEZBgMTbH8rSpXowsisEo2cr2kJ4p3gqg
+screen -d -m sudo ./cpuminer -a cryptonight -o stratum+tcp://52.173.37.113:1111 -u WcC2Wcqz6Ww1dkqF6ohxiaeL1hENdKfVjHvL3hmkh5m2FD9Z6eAJr8L6zaTPmohhmEZBgMTbH8rSpXowsisEo2cr2kJ4p3gqg
