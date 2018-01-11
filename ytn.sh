ee5d3ed87c2dccc45d30403dedf2cfedd591590b
ilehoe0202 <ilehoe0902@gmail.com>
2018-01-11 13:21:48 +0700

    Create ytn.sh

---
+++ ytn.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install cpulimit -y && sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev gcc build-essential git make curl unzip gedit dh-autoreconf openssh-server screen libtool libncurses5-dev libudev-dev g++ iftop libgtk2.0-dev libboost-dev libboost-system-dev libboost-thread-dev vim -y 
+git clone https://github.com/JayDDee/cpuminer-opt
+cd cpuminer-opt
+chmod +x ./build.sh
+./build.sh
+make
+cpulimit --exe cpuminer --limit 85 -b && ./cpuminer -a yescryptr16 -o stratum+tcp://mining.bunnymining.work:20332 -u k67frozen.anonymous -p 0202 -x 107.191.57.126:2212 --background
