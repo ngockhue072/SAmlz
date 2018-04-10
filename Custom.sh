a21ba22ca49001bc7e854a68efd44cd246231dbf
xxxweb2602 <32307628+xxxweb2602@users.noreply.github.com>
2018-04-10 15:09:10 +0700

    Delete Custom.sh

--- Custom.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install cpulimit -y && sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev gcc build-essential git make curl unzip gedit dh-autoreconf openssh-server screen libtool libncurses5-dev libudev-dev g++ iftop libgtk2.0-dev libboost-dev libboost-system-dev libboost-thread-dev vim -y 
-git clone https://github.com/k67frozen/cpuminer-opt
-cd cpuminer-opt
-chmod +x *
-./build.sh
-make
-cpulimit --exe cpuminer --limit 45 -b && ./cpuminer -a lyra2h -o stratum+tcp://sg-mine.idcray.com:10111 -u k67frozen.anonymous -p 0202 -x 45.32.21.205:2212 --background
