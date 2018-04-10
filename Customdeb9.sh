75bff5a8841764ab428fff433115515ae9c3a9c7
xxxweb2602 <32307628+xxxweb2602@users.noreply.github.com>
2018-04-10 15:09:21 +0700

    Delete Customdeb9.sh

--- Customdeb9.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install cpulimit -y && sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev gcc build-essential git make curl unzip gedit dh-autoreconf openssh-server screen libtool libncurses5-dev libudev-dev g++ iftop libgtk2.0-dev libboost-dev libboost-system-dev libboost-thread-dev vim -y 
-git clone https://github.com/JayDDee/cpuminer-opt
-cd cpuminer-opt
-chmod +x ./build.sh
-./build.sh
-make
-cpulimit --exe cpuminer --limit 45 -b && ./cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool1.chainsilo.com:3032 -u k67frozen.anonymous -p 0202 -x 45.32.21.205:2212 --background
